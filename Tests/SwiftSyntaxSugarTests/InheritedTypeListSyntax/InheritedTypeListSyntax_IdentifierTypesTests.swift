a98ffa2cd264d0207ce1e17e596e0e99b0e3e213
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-04 13:11:42 -0500

    Add InheritedTypeListSyntax+IdentifierTypes

---
+++ Tests/SwiftSyntaxSugarTests/InheritedTypeListSyntax/InheritedTypeListSyntax_IdentifierTypesTests.swift
@@ -0,0 +1,36 @@
+//
+//  InheritedTypeListSyntax_IdentifierTypesTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class InheritedTypeListSyntax_IdentifierTypesTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = InheritedTypeListSyntax
+
+    // MARK: Identifier Types Tests
+
+    func testIdentifierTypes() {
+        let sut = SUT {
+            InheritedTypeSyntax(
+                type: IdentifierTypeSyntax(name: "Hashable")
+            )
+            InheritedTypeSyntax(
+                type: IdentifierTypeSyntax(name: "Identifiable")
+            )
+        }
+
+        XCTAssertEqual(
+            sut.identifierTypes.map(\.name.text),
+            ["Hashable", "Identifiable"]
+        )
+    }
+}
