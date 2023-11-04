b5d03bff69d3f3e49a2242e4ece04d93c34c84e2
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-04 01:44:19 -0500

    Add TypeSyntax+Describing

---
+++ Tests/SwiftSyntaxSugarTests/TypeSyntax/TypeSyntax_DescribingTests.swift
@@ -0,0 +1,35 @@
+//
+//  TypeSyntax_DescribingTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class TypeSyntax_DescribingTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = TypeSyntax
+
+    // MARK: Initializer Tests
+
+    func testInitDescribingSubject() {
+        let sut = SUT(describing: Int.self)
+
+        XCTAssertEqual(sut.description, "Int")
+    }
+
+    // MARK: Static Property Tests
+
+    func testStaticProperties() {
+        XCTAssertEqual(SUT.bool.description, "Bool")
+        XCTAssertEqual(SUT.int.description, "Int")
+        XCTAssertEqual(SUT.string.description, "String")
+        XCTAssertEqual(SUT.void.description, "Void")
+    }
+}
