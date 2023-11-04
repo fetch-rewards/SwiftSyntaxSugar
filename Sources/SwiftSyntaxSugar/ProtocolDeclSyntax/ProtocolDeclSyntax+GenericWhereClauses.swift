a663c90f3c705d35e7ddd4d5b6e8a1c68adddcfc
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-04 13:25:31 -0500

    Add ProtocolDeclSyntax extensions

---
+++ Sources/SwiftSyntaxSugar/ProtocolDeclSyntax/ProtocolDeclSyntax+GenericWhereClauses.swift
@@ -0,0 +1,33 @@
+//
+//  ProtocolDeclSyntax+GenericWhereClauses.swift
+//  SwiftSyntaxSugar
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+
+extension ProtocolDeclSyntax {
+
+    /// The protocol's generic where clauses (the clause from the protocol
+    /// declaration and the clauses from the associated type declarations).
+    public var genericWhereClauses: [GenericWhereClauseSyntax] {
+        var genericWhereClauses: [GenericWhereClauseSyntax] = []
+
+        if let genericWhereClause = self.genericWhereClause {
+            genericWhereClauses.append(genericWhereClause)
+        }
+
+        for associatedTypeDeclaration in self.associatedTypeDeclarations {
+            guard 
+                let genericWhereClause = associatedTypeDeclaration.genericWhereClause
+            else {
+                continue
+            }
+
+            genericWhereClauses.append(genericWhereClause)
+        }
+
+        return genericWhereClauses
+    }
+}
