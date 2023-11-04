700bb7ffae30502221c0caa3d71fbd986b3006b9
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-04 00:55:19 -0500

    Add DeclModifierSyntax+AccessLevel

---
+++ Sources/SwiftSyntaxSugar/DeclModifierSyntax/DeclModifierSyntax+AccessLevel.swift
@@ -0,0 +1,18 @@
+//
+//  DeclModifierSyntax+AccessLevel.swift
+//  SwiftSyntaxSugar
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+
+extension DeclModifierSyntax {
+
+    /// A boolean indicating if the modifier is an access level modifier.
+    public var isAccessLevel: Bool {
+        AccessLevelSyntax.allCases.contains { accessLevel in
+            self.name.text == accessLevel.token.text
+        }
+    }
+}
