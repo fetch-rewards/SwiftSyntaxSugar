7cfc3bfdccb7b66485edd1079b2f61d25030f94d
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-04 12:47:37 -0500

    Add FunctionTypeSyntax+Escaping

---
+++ Tests/SwiftSyntaxSugarTests/FunctionTypeSyntax/FunctionTypeSyntax_EscapingTests.swift
@@ -0,0 +1,32 @@
+//
+//  FunctionTypeSyntax_EscapingTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class FunctionTypeSyntax_EscapingTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = FunctionTypeSyntax
+
+    // MARK: Escaping Tests
+
+    func testEscaping() {
+        let sut = SUT(
+            parameters: TupleTypeElementListSyntax(),
+            returnClause: ReturnClauseSyntax(type: .bool)
+        )
+
+        XCTAssertEqual(
+            sut.escaping().description,
+            "@escaping()->Bool"
+        )
+    }
+}
