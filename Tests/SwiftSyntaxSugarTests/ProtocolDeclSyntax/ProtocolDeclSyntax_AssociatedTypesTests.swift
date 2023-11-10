3ad2ce5283b7b2c04aa3df0ae0fe012a1d938760
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2023-11-09 22:37:16 -0600

    Add ProtocolDeclSyntax_AssociatedTypesTests

--- Tests/SwiftSyntaxSugarTests/ProtocolDeclSyntax/ProtocolDeclSyntax_AssociatedTypesTests.swift
+++ Tests/SwiftSyntaxSugarTests/ProtocolDeclSyntax/ProtocolDeclSyntax_AssociatedTypesTests.swift
@@ -15,4 +15,32 @@ final class ProtocolDeclSyntax_AssociatedTypesTests: XCTestCase {
     // MARK: Typealiases
 
     typealias SUT = ProtocolDeclSyntax
+
+    // MARK: Associated Type Declarations Tests
+
+    func testAssociatedTypeDeclarationsWithEmptyMemberBlock() {
+        let sut = SUT(name: "SUT") {}
+
+        XCTAssertTrue(sut.associatedTypeDeclarations.isEmpty)
+    }
+
+    func testAssociatedTypeDeclarationsWithNonEmptyMemberBlockWithAssociatedTypes() {
+        let sut = SUT(name: "SUT") {
+            VariableDeclSyntax(.var, name: "a")
+            AssociatedTypeDeclSyntax(name: "A")
+            VariableDeclSyntax(.var, name: "b")
+            AssociatedTypeDeclSyntax(name: "B")
+        }
+
+        XCTAssertEqual(sut.associatedTypeDeclarations.count, 2)
+    }
+
+    func testAssociatedTypeDeclarationsWithNonEmptyMemberBlockWithoutAssociatedTypes() {
+        let sut = SUT(name: "SUT") {
+            VariableDeclSyntax(.var, name: "a")
+            VariableDeclSyntax(.var, name: "b")
+        }
+
+        XCTAssertTrue(sut.associatedTypeDeclarations.isEmpty)
+    }
 }
