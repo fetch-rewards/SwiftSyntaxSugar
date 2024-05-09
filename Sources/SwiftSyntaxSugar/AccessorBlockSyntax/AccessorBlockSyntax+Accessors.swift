88e61f0380526b3ccc5e72bdd22e4b2dc848ed43
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-05-09 14:40:22 -0500

    Add AccessorBlockSyntax+Accessors (#3)

---
+++ Sources/SwiftSyntaxSugar/AccessorBlockSyntax/AccessorBlockSyntax+Accessors.swift
@@ -0,0 +1,55 @@
+//
+//  AccessorBlockSyntax+Accessors.swift
+//  SwiftSyntaxSugar
+//
+//  Created by Gray Campbell on 11/3/23.
+//
+
+import SwiftSyntax
+
+extension AccessorBlockSyntax {
+
+    // MARK: Accessors
+
+    /// The accessor block's `get` accessor declaration, if there is one.
+    public var getAccessorDeclaration: AccessorDeclSyntax? {
+        guard case let .accessors(accessorDeclarations) = self.accessors else {
+            return nil
+        }
+
+        return accessorDeclarations.first { accessorDeclaration in
+            let getToken: TokenSyntax = .keyword(.get)
+            let accessorSpecifier = accessorDeclaration.accessorSpecifier
+
+            return accessorSpecifier.text == getToken.text
+        }
+    }
+
+    /// The accessor block's `set` accessor declaration, if there is one.
+    public var setAccessorDeclaration: AccessorDeclSyntax? {
+        guard case let .accessors(accessorDeclarations) = self.accessors else {
+            return nil
+        }
+
+        return accessorDeclarations.first { accessorDeclaration in
+            let setToken: TokenSyntax = .keyword(.set)
+            let accessorSpecifier = accessorDeclaration.accessorSpecifier
+
+            return accessorSpecifier.text == setToken.text
+        }
+    }
+
+    // MARK: Contains
+
+    /// A boolean indicating if the accessor block contains a `get` accessor
+    /// declaration.
+    public var containsGetAccessor: Bool {
+        self.getAccessorDeclaration != nil
+    }
+
+    /// A boolean indicating if the accessor block contains a `set` accessor
+    /// declaration.
+    public var containsSetAccessor: Bool {
+        self.setAccessorDeclaration != nil
+    }
+}
---
+++ Tests/SwiftSyntaxSugarTests/AccessorBlockSyntax/AccessorBlockSyntax_AccessorsTests.swift
@@ -0,0 +1,115 @@
+//
+//  AccessorBlockSyntax_AccessorsTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/3/23.
+//
+
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class AccessorBlockSyntax_AccessorsTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = AccessorBlockSyntax
+
+    // MARK: Get Accessor Declaration Tests
+
+    func testGetAccessorDeclarationWithAccessorsWithGetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.get))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertNotNil(sut.getAccessorDeclaration)
+    }
+
+    func testGetAccessorDeclarationWithAccessorsWithoutGetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.set))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertNil(sut.getAccessorDeclaration)
+    }
+
+    func testGetAccessorDeclarationWithGetter() {
+        let sut = SUT(accessors: .getter(CodeBlockItemListSyntax()))
+
+        XCTAssertNil(sut.getAccessorDeclaration)
+    }
+
+    // MARK: Set Accessor Declaration Tests
+
+    func testSetAccessorDeclarationWithAccessorsWithSetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.set))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertNotNil(sut.setAccessorDeclaration)
+    }
+
+    func testSetAccessorDeclarationWithAccessorsWithoutSetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.get))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertNil(sut.setAccessorDeclaration)
+    }
+
+    func testSetAccessorDeclarationWithGetter() {
+        let sut = SUT(accessors: .getter(CodeBlockItemListSyntax()))
+
+        XCTAssertNil(sut.setAccessorDeclaration)
+    }
+
+    // MARK: Contains Get Accessor Tests
+
+    func testContainsGetAccessorWithAccessorsWithGetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.get))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertTrue(sut.containsGetAccessor)
+    }
+
+    func testContainsGetAccessorWithAccessorsWithoutGetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.set))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertFalse(sut.containsGetAccessor)
+    }
+
+    func testContainsGetAccessorWithGetter() {
+        let sut = SUT(accessors: .getter(CodeBlockItemListSyntax()))
+
+        XCTAssertFalse(sut.containsGetAccessor)
+    }
+
+    // MARK: Contains Set Accessor Tests
+
+    func testContainsSetAccessorWithAccessorsWithSetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.set))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertTrue(sut.containsSetAccessor)
+    }
+
+    func testContainsSetAccessorWithAccessorsWithoutSetAccessor() {
+        let accessor = AccessorDeclSyntax(accessorSpecifier: .keyword(.get))
+        let accessors = AccessorDeclListSyntax { accessor }
+        let sut = SUT(accessors: .accessors(accessors))
+
+        XCTAssertFalse(sut.containsSetAccessor)
+    }
+
+    func testContainsSetAccessorWithGetter() {
+        let sut = SUT(accessors: .getter(CodeBlockItemListSyntax()))
+
+        XCTAssertFalse(sut.containsSetAccessor)
+    }
+}
