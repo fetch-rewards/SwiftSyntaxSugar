dce2bcc09a4279aa56e02ae5b56484764cf6ac17
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-07-18 09:59:50 -0500

    Add DeclModifierSyntax+AccessLevel (#5)

---
+++ Tests/SwiftSyntaxSugarTests/DeclModifierSyntax/DeclModifierSyntax_AccessLevelTests.swift
@@ -0,0 +1,38 @@
+//
+//  DeclModifierSyntax_AccessLevelTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class DeclModifierSyntax_AccessLevelTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = DeclModifierSyntax
+
+    // MARK: Is Access Level Tests
+
+    func testIsAccessLevelWithAccessLevelModifiers() {
+        for accessLevel in AccessLevelSyntax.allCases {
+            let sut = SUT(name: accessLevel.token)
+
+            XCTAssertTrue(sut.isAccessLevel)
+        }
+    }
+
+    func testIsAccessLevelWithNonAccessLevelModifiers() {
+        let keywords: [Keyword] = [.class, .lazy, .static]
+
+        for keyword in keywords {
+            let sut = SUT(name: .keyword(keyword))
+
+            XCTAssertFalse(sut.isAccessLevel)
+        }
+    }
+}
