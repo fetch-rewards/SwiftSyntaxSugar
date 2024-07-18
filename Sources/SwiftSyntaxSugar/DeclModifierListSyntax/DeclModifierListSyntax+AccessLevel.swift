62282f84bb69e2864754442076c6c37a981fc395
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-07-18 10:12:55 -0500

    Add DeclModifierListSyntax+AccessLevel (#6)

---
+++ Sources/SwiftSyntaxSugar/DeclModifierListSyntax/DeclModifierListSyntax+AccessLevel.swift
@@ -0,0 +1,26 @@
+//
+//  DeclModifierListSyntax+AccessLevel.swift
+//  SwiftSyntaxSugar
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+
+extension DeclModifierListSyntax {
+
+    /// The first access level modifier found in the modifier list, or
+    /// `.internal` if none exists.
+    public var accessLevel: AccessLevelSyntax {
+        guard
+            let accessLevelModifier = self.first(where: \.isAccessLevel),
+            let accessLevel = AccessLevelSyntax(
+                rawValue: accessLevelModifier.name.text
+            )
+        else {
+            return .internal
+        }
+
+        return accessLevel
+    }
+}
---
+++ Tests/SwiftSyntaxSugarTests/DeclModifierListSyntax/DeclModifierListSyntax_AccessLevelTests.swift
@@ -0,0 +1,39 @@
+//
+//  DeclModifierListSyntax_AccessLevelTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class DeclModifierListSyntax_AccessLevelTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = DeclModifierListSyntax
+
+    // MARK: Access Level Tests
+
+    func testAccessLevelWithExplicitAccessLevels() {
+        for accessLevel in AccessLevelSyntax.allCases {
+            let sut = SUT {
+                accessLevel.modifier
+                DeclModifierSyntax(name: .keyword(.static))
+            }
+
+            XCTAssertEqual(sut.accessLevel, accessLevel)
+        }
+    }
+
+    func testAccessLevelWithImplicitInternalAccessLevel() {
+        let sut = SUT {
+            DeclModifierSyntax(name: .keyword(.static))
+        }
+
+        XCTAssertEqual(sut.accessLevel, .internal)
+    }
+}
