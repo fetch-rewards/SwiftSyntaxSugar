9a51d9b0d0a6e3d8822345e3cd7bf6a59fd14710
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-07-30 15:22:28 -0500

    Add FunctionDeclSyntax extensions (#9)

---
+++ Tests/SwiftSyntaxSugarTests/FunctionDeclSyntax/FunctionDeclSyntax_AccessLevelTests.swift
@@ -0,0 +1,92 @@
+//
+//  FunctionDeclSyntax_AccessLevelTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class FunctionDeclSyntax_AccessLevelTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = FunctionDeclSyntax
+
+    // MARK: Access Level Tests
+
+    func testAccessLevelWithExplicitAccessLevels() {
+        for accessLevel in AccessLevelSyntax.allCases {
+            let sut = SUT(
+                modifiers: DeclModifierListSyntax { accessLevel.modifier },
+                name: "sut",
+                signature: FunctionSignatureSyntax(
+                    parameterClause: FunctionParameterClauseSyntax {}
+                )
+            )
+
+            XCTAssertEqual(sut.accessLevel, accessLevel)
+        }
+    }
+
+    func testAccessLevelWithImplicitInternalAccessLevel() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {}
+            )
+        )
+
+        XCTAssertEqual(sut.accessLevel, .internal)
+    }
+
+    // MARK: With Access Level Tests
+
+    func testWithAccessLevelWithExplicitAccessLevels() {
+        for oldAccessLevel in AccessLevelSyntax.allCases {
+            for newAccessLevel in AccessLevelSyntax.allCases {
+                let sut = SUT(
+                    modifiers: DeclModifierListSyntax {
+                        oldAccessLevel.modifier
+                        DeclModifierSyntax(name: .keyword(.static))
+                    },
+                    name: "sut",
+                    signature: FunctionSignatureSyntax(
+                        parameterClause: FunctionParameterClauseSyntax {}
+                    )
+                )
+                .withAccessLevel(newAccessLevel)
+
+                XCTAssertEqual(sut.accessLevel, newAccessLevel)
+                XCTAssertEqual(
+                    sut.modifiers.count,
+                    newAccessLevel == .internal ? 1 : 2
+                )
+            }
+        }
+    }
+
+    func testWithAccessLevelWithImplicitInternalAccessLevel() {
+        for newAccessLevel in AccessLevelSyntax.allCases {
+            let sut = SUT(
+                modifiers: DeclModifierListSyntax {
+                    DeclModifierSyntax(name: .keyword(.static))
+                },
+                name: "sut",
+                signature: FunctionSignatureSyntax(
+                    parameterClause: FunctionParameterClauseSyntax {}
+                )
+            )
+            .withAccessLevel(newAccessLevel)
+
+            XCTAssertEqual(sut.accessLevel, newAccessLevel)
+            XCTAssertEqual(
+                sut.modifiers.count,
+                newAccessLevel == .internal ? 1 : 2
+            )
+        }
+    }
+}
---
+++ Tests/SwiftSyntaxSugarTests/FunctionDeclSyntax/FunctionDeclSyntax_ToFunctionTypeSyntaxTests.swift
@@ -0,0 +1,177 @@
+//
+//  FunctionDeclSyntax_ToFunctionTypeSyntaxTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/4/23.
+//
+
+import SwiftSyntax
+import XCTest
+
+@testable import SwiftSyntaxSugar
+
+final class FunctionDeclSyntax_ToFunctionTypeSyntaxTests: XCTestCase {
+
+    // MARK: Typealiases
+
+    typealias SUT = FunctionDeclSyntax
+
+    // MARK: To FunctionTypeSyntax Tests
+
+    func testToFunctionTypeSyntaxWithParameters() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {
+                    FunctionParameterSyntax(firstName: "integer", type: .int)
+                    FunctionParameterSyntax(
+                        firstName: "closure",
+                        type: FunctionTypeSyntax(
+                            parameters: TupleTypeElementListSyntax {
+                                TupleTypeElementSyntax(type: .string)
+                                TupleTypeElementSyntax(type: .bool)
+                            },
+                            returnClause: ReturnClauseSyntax(type: .void)
+                        )
+                    )
+                }
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "(Int,(String,Bool)->Void)->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithAttributedParameters() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {
+                    FunctionParameterSyntax(firstName: "integer", type: .int)
+                    FunctionParameterSyntax(
+                        firstName: "closure",
+                        type: AttributedTypeSyntax(
+                            attributes: AttributeListSyntax {
+                                AttributeSyntax(
+                                    atSign: .atSignToken(),
+                                    attributeName: IdentifierTypeSyntax(
+                                        name: .keyword(.escaping)
+                                    )
+                                )
+                            },
+                            baseType: FunctionTypeSyntax(
+                                parameters: TupleTypeElementListSyntax {
+                                    TupleTypeElementSyntax(type: .string)
+                                    TupleTypeElementSyntax(type: .bool)
+                                },
+                                returnClause: ReturnClauseSyntax(type: .void)
+                            )
+                        )
+                    )
+                }
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "(Int,(String,Bool)->Void)->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithoutParameters() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {}
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithAsyncSpecifier() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {},
+                effectSpecifiers: FunctionEffectSpecifiersSyntax(
+                    asyncSpecifier: .keyword(.async)
+                )
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()async->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithThrowsSpecifier() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {},
+                effectSpecifiers: FunctionEffectSpecifiersSyntax(
+                    throwsSpecifier: .keyword(.throws)
+                )
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()throws->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithAsyncAndThrowsSpecifiers() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {},
+                effectSpecifiers: FunctionEffectSpecifiersSyntax(
+                    asyncSpecifier: .keyword(.async),
+                    throwsSpecifier: .keyword(.throws)
+                )
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()asyncthrows->Void"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithReturnValue() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {},
+                returnClause: ReturnClauseSyntax(type: .bool)
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()->Bool"
+        )
+    }
+
+    func testToFunctionTypeSyntaxWithoutReturnValue() {
+        let sut = SUT(
+            name: "sut",
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {}
+            )
+        )
+
+        XCTAssertEqual(
+            sut.toFunctionTypeSyntax().description,
+            "()->Void"
+        )
+    }
+}
