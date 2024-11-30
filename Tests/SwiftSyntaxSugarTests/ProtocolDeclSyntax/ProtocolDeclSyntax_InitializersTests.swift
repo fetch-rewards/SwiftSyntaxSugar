3ba18aa830c96b98d1f42a8782d1d34ca9bd29d3
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-11-30 12:37:09 -0600

    Add initializerDeclarations computed property to ProtocolDeclSyntax

---
+++ Tests/SwiftSyntaxSugarTests/ProtocolDeclSyntax/ProtocolDeclSyntax_InitializersTests.swift
@@ -0,0 +1,63 @@
+//
+//  ProtocolDeclSyntax_InitializersTests.swift
+//  SwiftSyntaxSugarTests
+//
+//  Created by Gray Campbell on 11/30/24.
+//
+
+import SwiftSyntax
+import Testing
+@testable import SwiftSyntaxSugar
+
+struct ProtocolDeclSyntax_InitializersTests {
+
+    // MARK: Typealiases
+
+    typealias SUT = ProtocolDeclSyntax
+
+    // MARK: Initializer Declarations Tests
+
+    @Test
+    func initializerDeclarations() {
+        let sut = SUT(name: .identifier("Initializers")) {
+            self.initializerDeclaration()
+            self.propertyDeclaration(name: "property1")
+            self.initializerDeclaration()
+            self.methodDeclaration(name: "method1")
+            self.initializerDeclaration()
+            self.propertyDeclaration(name: "property2")
+            self.initializerDeclaration()
+            self.methodDeclaration(name: "method2")
+            self.initializerDeclaration()
+            self.propertyDeclaration(name: "property3")
+            self.initializerDeclaration()
+        }
+
+        #expect(sut.initializerDeclarations.count == 6)
+    }
+}
+
+// MARK: - Helpers
+
+extension ProtocolDeclSyntax_InitializersTests {
+    private func initializerDeclaration() -> InitializerDeclSyntax {
+        InitializerDeclSyntax(
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {}
+            )
+        ) {}
+    }
+
+    private func methodDeclaration(name: String) -> FunctionDeclSyntax {
+        FunctionDeclSyntax(
+            name: .identifier(name),
+            signature: FunctionSignatureSyntax(
+                parameterClause: FunctionParameterClauseSyntax {}
+            )
+        ) {}
+    }
+
+    private func propertyDeclaration(name: PatternSyntax) -> VariableDeclSyntax {
+        VariableDeclSyntax(.var, name: name)
+    }
+}
