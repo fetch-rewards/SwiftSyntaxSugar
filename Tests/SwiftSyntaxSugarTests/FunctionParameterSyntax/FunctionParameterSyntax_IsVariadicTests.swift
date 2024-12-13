6eb06616020bbf06f645d37d5083b7134a892c5a
Gray Campbell <12163070+graycampbell@users.noreply.github.com>
2024-12-13 13:47:18 -0600

    Fix FunctionParameterListSyntax.toTupleTypeSyntax variadic parameters (#23)

---
+++ Tests/SwiftSyntaxSugarTests/FunctionParameterSyntax/FunctionParameterSyntax_IsVariadicTests.swift
@@ -0,0 +1,42 @@
+//
+//  FunctionParameterSyntax_IsVariadicTests.swift
+//  SwiftSyntaxSugar
+//
+//  Created by Gray Campbell on 12/9/24.
+//
+
+import SwiftSyntax
+import Testing
+@testable import SwiftSyntaxSugar
+
+struct FunctionParameterSyntax_IsVariadicTests {
+
+    // MARK: Typealiases
+
+    typealias SUT = FunctionParameterSyntax
+
+    // MARK: Is Variadic Tests
+
+    @Test
+    func isVariadicWhenEllipsisIsNil() {
+        let sut = SUT(
+            firstName: .identifier("parameter"),
+            colon: .colonToken(),
+            type: .int
+        )
+
+        #expect(!sut.isVariadic)
+    }
+
+    @Test
+    func isVariadicWhenEllipsisIsNotNil() {
+        let sut = SUT(
+            firstName: .identifier("parameter"),
+            colon: .colonToken(),
+            type: .int,
+            ellipsis: .ellipsisToken()
+        )
+
+        #expect(sut.isVariadic)
+    }
+}
